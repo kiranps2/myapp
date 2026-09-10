eb21c9971d476b36d3669206c815f0a044bb7499
kiran ps <kiranpsofficial1@gmail.com>
2026-09-10 10:26:36 +0530

    Refactor DayTwoHWPhotography controller and update photography.html to use new image source
    - Added a blank line for better code readability in DayTwoHWPhotography.java
    - Removed unused image file 'images (1).jpg' from static resources
    - Updated photography.html to reference the new image 'camera.jpg'
    - Added 'camera.jpg' to static images directory

--- src/main/java/com/example/myapp/Controller/DayTwoHWPhotography.java
+++ src/main/java/com/example/myapp/Controller/DayTwoHWPhotography.java
@@ -1,6 +1,7 @@
 package com.example.myapp.Controller;
 import org.springframework.stereotype.Controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DayTwoHWPhotography {
