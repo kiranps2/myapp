7c3a28c6dce301c287779c97da3bbc290f779d23
kiran ps <kiranpsofficial1@gmail.com>
2026-09-10 09:07:52 +0530

    Add DayTwoHWPhotography controller, photography.html template, and photographyjs.js script

---
+++ src/main/java/com/example/myapp/Controller/DayTwoHWPhotography.java
@@ -0,0 +1,16 @@
+package com.example.myapp.Controller;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class DayTwoHWPhotography {
+	@GetMapping("/start2")
+    public String start2() {
+        return "redirect:/photography";
+    }
+	
+	@GetMapping("/photography")
+    public String photography() {
+        return "photography";
+    }
+}
---
+++ src/main/resources/static/js/photographyjs.js
@@ -0,0 +1,3 @@
+function showstring(){
+	document.write("Thanks for clicking the camera!");
+}
